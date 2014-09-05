1b571829e539d16c91e86ba85cbe95cfa15e91ac
simendag <s236376@stud.hioa.no>
2014-09-05 18:15:56 +0200

    Update HeltallSummeringMain.java
    while + while-debug

--- HeltallSummeringMain.java
+++ HeltallSummeringMain.java
@@ -1,31 +1,26 @@
 // Dette er applikasjonens main-fil. Den inneholder main-metoden.
+import javax.swing.JOptionPane;
+
 public class HeltallSummeringMain {
   
-  public static void main(String[] args) { // Start, main-metoden
+  public static void main(String[] args) { // Start, main-metoden.
   
-  System.out.println("Hello world!");
+    // Lager objekt av JOptionPane for mer oversiktlig kode.
+    JOptionPane j = new JOptionPane();
     
+    // Deklarer variabler.
     long nedre, øvre;
     boolean godkjent;
     
-    while(<godkjent>)
-    {
-      if(øvre <= nedre)
-        {
-          
-        <melding>
-        
-        }else if(øvre > nedre)
-        {
-          godkjent = true;
-          
-        }else
-        
-        System.out.println("noen rart skjedde");
-      
+    while (øvre <= nedre) {
+      j.showMessageDialog(null, "Øvre grensetall må være større enn nedre grensetall.");
+      nedre = Long.parseLong(j.showInputDialog("Les inn nytt heltall som nedre grense: "));
+      øvre = Long.parseLong(j.showInputDialog("Les inn nytt heltall som øvre grense: "));
     }
     
+    System.out.println("While løkken ble kjørt.") // Debug, sjekker om while-løkken ble ferdig.
     
-  } // Slutt, main-metoden
+    
+  } // Slutt, main-metoden.
   
-} // Slutt, HeltallSummeringMain
+} // Slutt, HeltallSummeringMain.
