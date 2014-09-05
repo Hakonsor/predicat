3a28b97c92742982bc9400c94bd75d8c34ed0cac
simendag <s236376@stud.hioa.no>
2014-09-05 18:50:23 +0200

    Update HeltallSummeringMain.java

--- HeltallSummeringMain.java
+++ HeltallSummeringMain.java
@@ -19,10 +19,15 @@ public class HeltallSummeringMain {
     
     System.out.println("While løkken ble kjørt.") // Debug, sjekker om while-løkken ble ferdig.
     
-    for(long i = (nedre-1); i == øvre; i++){
-      tekst += (i+"+");
-      sum += i;
-      
+    for(long i = (nedre-1); i == øvre; i++){ // Start, for-løkke.
+      if (i + 1 == øvre) { // Start, slutt-test som legger til sum i strengen.
+        sum += i;
+        tekst += i + " = " + sum;
+      }
+      else { // Sørger for at man ser hvilke tall som blir lagt sammen før summen.
+        tekst += (i + " + "); 
+        sum += i;
+      } 
     }
     
     
