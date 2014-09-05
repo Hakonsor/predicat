b4f6485c6211f6dd5038926192d452ecb280126a
simendag <s236376@stud.hioa.no>
2014-09-05 19:01:08 +0200

    Update HeltallSummeringMain.java

--- HeltallSummeringMain.java
+++ HeltallSummeringMain.java
@@ -8,30 +8,38 @@ public class HeltallSummeringMain {
     // Lager objekt av JOptionPane for mer oversiktlig kode.
     JOptionPane j = new JOptionPane();
     
-    // Deklarer variabler.
+    // Deklarer variabler:
     long nedre, øvre, tall, sum;
     String tekst = "";
-    while (øvre <= nedre) {
+    
+    // Ber om heltall for øvre og nedre grenseverdier:
+    nedre = j.showInputDialog("Hva er den nedre " ) 
+    øvre = j.showInputDialog("Hva er den øvre " )
+    while (øvre <= nedre) { // Undersøker om øvre grenseverdi er mindre enn nedre grenseverdi.
       j.showMessageDialog(null, "Øvre grensetall må være større enn nedre grensetall.");
       nedre = Long.parseLong(j.showInputDialog("Les inn nytt heltall som nedre grense: "));
       øvre = Long.parseLong(j.showInputDialog("Les inn nytt heltall som øvre grense: "));
     }
     
-    System.out.println("While løkken ble kjørt.") // Debug, sjekker om while-løkken ble ferdig.
+    // Løkke for å lage String med tekst og sum:
+    for (long i = (nedre-1); i == øvre; i++){
     
-    for (long i = (nedre-1); i == øvre; i++){ // Start, for-løkke.
-      if (i + 1 == øvre) { // Start, slutt-test som legger til sum i strengen.
+      // Slutt-test som legger til sum i strengen.
+      if (i + 1 == øvre) { 
         sum += i;
         tekst += i + " = " + sum;
       }
-      else { // Sørger for at man ser hvilke tall som blir lagt sammen før summen.
+      
+      // Sørger for at man ser hvilke tall som blir lagt sammen før summen.
+      else { 
         tekst += (i + " + "); 
         sum += i;
         for (int k = 0; k==10; k++) { // Sørger for at det blir printet en ny linje i teksten hvert tiende tall.
           tekst += "\n";
         }
-      }
-    }
+      } 
+    } // Slutt på løkke for å lage String med tekst og sum.
+    
     
     
     
