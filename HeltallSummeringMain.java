77f5f3b1964ca50dbe7a807f0be6651959875385
simendag <s236376@stud.hioa.no>
2014-09-05 19:11:04 +0200

    Update HeltallSummeringMain.java

--- HeltallSummeringMain.java
+++ HeltallSummeringMain.java
@@ -5,49 +5,49 @@ import javax.swing.JOptionPane;
 
 // Deklarerer klassen:
 public class HeltallSummeringMain {
-  
+
   public static void main(String[] args) { // Start, main-metoden.
-  
+
     // Lager objekt av JOptionPane for mer oversiktlig kode.
     JOptionPane j = new JOptionPane();
-    
+
     // Deklarer variabler:
-    long nedre, øvre, tall, sum;
+    long nedre, øvre, tall, sum = 0;
     String tekst = "";
-    
+
     // Ber om heltall for øvre og nedre grenseverdier:
-    nedre = j.showInputDialog("Les inn heltall som nedre grense: ");
-    øvre = j.showInputDialog("Les inn heltall som øvre grense: ");
-    
+    nedre = Long.parseLong(j.showInputDialog("Les inn heltall som nedre grense: "));
+    øvre = Long.parseLong(j.showInputDialog("Les inn heltall som øvre grense: "));
+
     // Undersøker om øvre grenseverdi er mindre enn nedre grenseverdi.
-    while (øvre <= nedre) { 
+    while (øvre <= nedre) {
       j.showMessageDialog(null, "Øvre grensetall må være større enn nedre grensetall.");
       nedre = Long.parseLong(j.showInputDialog("Les inn nytt heltall som nedre grense: "));
       øvre = Long.parseLong(j.showInputDialog("Les inn nytt heltall som øvre grense: "));
     }
-    
+
     // Løkke for å lage String med tekst og sum:
     for (long i = (nedre-1); i == øvre; i++) {
-    
+
       // Slutt-test som legger til sum i strengen.
-      if (i + 1 == øvre) { 
+      if (i + 1 == øvre) {
         sum += i;
         tekst += i + " = " + sum;
       }
-      
+
       // Sørger for at man ser hvilke tall som blir lagt sammen før summen.
-      else { 
-        tekst += (i + " + "); 
+      else {
+        tekst += (i + " + ");
         sum += i;
         for (int k = 0; k==10; k++) { // Sørger for at det blir printet en ny linje i teksten hvert tiende tall.
           tekst += "\n";
         }
-      } 
+      }
     } // Slutt på løkke for å lage String med tekst og sum.
-    
+
     // Printer resultatet i vindu:
     j.showMessageDialog(null,tekst);
-    
+
   } // Slutt, main-metoden.
-  
+
 } // Slutt, HeltallSummeringMain.
