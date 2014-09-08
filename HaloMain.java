c90695d4bf036e4f570ac2705867ad7eaf0dfce9
Simen <s236376@stud.hioa.no>
2014-09-08 11:09:41 +0200

    Added JOptionPane object 'j'

--- HaloMain.java
+++ HaloMain.java
@@ -1,7 +1,9 @@
 import javax.swing.JOptionPane;
 
 public class HaloMain {
+
   public static void main(String... args) {
+  	JOptionPane j = new JOptionPane();
 
   } // Slutt på main-metoden
 } // Slutt på HaloMain
