8bd93c6dc57fff921dfa57b4a9f91d365a1b5ef1
Håkon Sørby <hakonsor@gmail.com>
2014-09-08 10:25:48 +0200

    java
    java uten løsning

---
+++ SirkelTest.java
@@ -0,0 +1,31 @@
+/*Nedenfor ser du en skisse av hovedprogrammet
+som du skal bruke til å teste ut klassen Sirkel.
+Din oppgave er også her å erstatte pseudokoden med java-instruksjoner. >*/
+
+<  nødvendig(e) import-setning(er) >
+public class SirkelTest
+{
+  public static void main(String[] args)
+  {
+    <  Les inn sirkelens radius fra brukeren. >
+
+    <  Så lenge radius er positiv:
+       Opprett et Sirkel-objekt, som ved opprettelsen blir tilført
+       den radius som brukeren har skrevet inn.
+       Summer radiusene ettehvert som de leses inn.
+       Skriv sirkelens radius, diameter, omkrets og areal ut på skjermen
+       ved å bruke en eller flere metoder i Sirkel-klassen.
+       Les inn sirkelens radius på nytt fra brukeren. >
+
+   <  Skriv ut den gjennomsnittelige radiusen for de innleste sirkelene.
+      Hvis det ikke er lest inn noen skal programmet gi beskjed om det. >
+
+  }
+} // end of class SirkelTest
+/*Innlesing av radius og utskrift av radius, diameter,
+omkrets og areal skal foretas gjentatte ganger,
+inntil det leses inn en verdi som gjør betingelsen usann.
+Bruk dialogvinduer til både innlesing og utskrift.
+(Utskrift av radius, diameter, omkrets og areal kan gjøres i en og samme dialogvindu.)
+NB! Klassene må skrives på hver sin fil.
+Opprett en mappe som heter Sirkel, og legg begge filene i den.*/
