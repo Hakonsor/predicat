910c8fa4ac618fb7a1a77bd6e2ac4c8e8df6a607
Simen <s236376@stud.hioa.no>
2014-09-08 11:11:48 +0200

    Added Halo object 's'

--- HaloMain.java
+++ HaloMain.java
@@ -4,6 +4,6 @@ public class HaloMain {
 
   public static void main(String... args) {
   	JOptionPane j = new JOptionPane();
-
+  	Halo s = new Halo(Double.parseDouble(j.showInputdialog("What is the radius of the circle?")));
   } // Slutt på main-metoden
 } // Slutt på HaloMain
